ae2d979ea56b4c77643c53a9762554fde887b15a
Stepn_V <stepan.vladovskiy@gmail.com>
2023-03-02 17:15:21 +0000

    fix: header wrong

--- packages/ui/src/components/header.tsx
+++ packages/ui/src/components/header.tsx
@@ -3,20 +3,5 @@ import React from "react";
 
 export function header() {
   return (
-    <XStack
-      f={1}
-      as="header"
-      position="absolute"
-      top='0'
-      py='2'
-      px='4'
-      boxShadow='0 0 4px rgba(0, 0, 0, 0.2)'
-    >
-      <nav>
-        <text>Home</text>
-        <text>About</text>
-        <text>Contact</text>
-      </nav>
-    </XStack>
   );
 } 
