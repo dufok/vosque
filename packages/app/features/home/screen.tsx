17f6da6afd52189b328b6f419a58d658e3f67d67
Stepn_V <stepan.vladovskiy@gmail.com>
2023-03-03 23:45:19 +0000

    feat: background in header

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -15,6 +15,7 @@ import { useLink } from "solito/link";
 import { trpc } from "../../utils/trpc";
 import { SignedIn, SignedOut, useAuth } from "../../utils/clerk";
 import '../../background.css';
+import { Header} from '@my/ui/src/components/header';
 
 
 export function HomeScreen() {
@@ -48,6 +49,7 @@ export function HomeScreen() {
 
   return (
     <YStack f={1} jc="flex-start" className="background-image" miw={500} space="$6">
+      <Header />
       <YStack>
           <Image
             als="center"
