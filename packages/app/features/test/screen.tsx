bb182c5bbf51cfc6b9a8083fa290459f354a904f
Stepn_V <stepan.vladovskiy@gmail.com>
2023-03-15 15:59:35 +0000

    feat: test user screen

--- packages/app/features/test/screen.tsx
+++ packages/app/features/test/screen.tsx
@@ -1,122 +1,42 @@
+import React from "react";
+import { useRouter } from "next/router";
 import {
   YStack,
   H1,
-  H3,
-  XStack,
-  ZStack,
-  Image,
-  Square,
-  Button
+  Paragraph,
+  Button,
 } from "@my/ui";
-import React from "react";
-//add background css
-import '@my/ui/src/background.css';
-//add Font style
-import '@my/ui/src/styles.css'
-//add Header
-import { Header} from '@my/ui/src/components/HeaderComp';
-
+import "@my/ui/src/background.css";
+import "@my/ui/src/styles.css";
+import { Header } from "@my/ui/src/components/HeaderComp";
+import { UserDetailScreen } from "app/features/user/detail-screen";
+import { trpc } from "../../utils/trpc";
 
 export function testScreen() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data: user, isLoading, error } = trpc.entry.all.useQuery(["user.current", { id: parseInt(id as string) }]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <YStack f={1} className="background-image" miw={500} space="$6">
-      <YStack ai="center">
-        <H1 ta="center" fos={70} tt="uppercase" fow="$4" mt="$10" mb="$10" color="$color2" style={{ fontFamily: 'VosqueStyle' }}>
-          Разговорники
-        </H1>
-        <XStack m="$5" jc='space-around' alignItems="center" fw='wrap' maw={1600}>
-          <YStack
-            position="relative"
-            w={500} h={300}
-            >
-            <Image
-              src="https://placekitten.com/200/300"
-              width={400}
-              height={200}
-              hoverStyle={{ opacity: 0.5 }}
-            />
-            <H3
-              position="absolute"
-              zi={50}
-              tt="uppercase"
-            >
-              аренда жилья
-            </H3>
-          </YStack>
-          <YStack
-            position="relative"
-            w={500} h={300}
-            >
-            <Image
-              src="https://placekitten.com/200/300"
-              width={400}
-              height={200}
-              hoverStyle={{ opacity: 0.5 }}
-            />
-            <H3
-              position="absolute"
-              zi={50}
-              tt="uppercase"
-            >
-              оформление документов
-            </H3>
-          </YStack>
-          <YStack
-            position="relative"
-            w={500} h={300}
-            >
-            <Image
-              src="https://placekitten.com/200/300"
-              width={400}
-              height={200}
-              hoverStyle={{ opacity: 0.5 }}
-            />
-            <H3
-              position="absolute"
-              zi={50}
-              tt="uppercase"
-            >
-              в ресторане
-            </H3>
-          </YStack>
-          <YStack
-            position="relative"
-            w={500} h={300}
-            >
-            <Image
-              src="https://placekitten.com/200/300"
-              width={400}
-              height={200}
-              hoverStyle={{ opacity: 0.5 }}
-            />
-            <H3
-              position="absolute"
-              zi={50}
-              tt="uppercase"
-            >
-              квир/на дейтинге
-            </H3>
-          </YStack>
-          <YStack
-            position="relative"
-            w={500} h={300}
-            >
-            <Image
-              src="https://placekitten.com/200/300"
-              width={400}
-              height={200}
-              hoverStyle={{ opacity: 0.5 }}
-            />
-            <H3
-              position="absolute"
-              zi={50}
-              tt="uppercase"
-            >
-              ругательства
-            </H3>
-          </YStack>
-        </XStack>
-      </YStack>
+    <YStack f={1} ai="center" className="background-image" miw={400} space="$6">
+      <Header />
+      <H1>Test Screen</H1>
+      <Paragraph>
+        {user ? (
+          <UserDetailScreen id={user.id} email={user.email} />
+        ) : (
+          // Replace this with your sign-in link as needed
+          <Button href="/signin">Sign In</Button>
+        )}
+      </Paragraph>
     </YStack>
   );
 }
--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -6,13 +6,14 @@ import { useLink } from 'solito/link'
 
 const { useParam } = createParam<{ id: string }>()
 
-export function UserDetailScreen() {
+export function UserDetailScreen({ email }) {
   const [id] = useParam('id')
   const linkProps = useLink({ href: '/' })
 
   return (
     <YStack f={1} jc="center" ai="center" space>
       <Paragraph ta="center" fow="800">{`User ID: ${id}`}</Paragraph>
+      <Paragraph ta="center" fow="800">{`User Email: ${email}`}</Paragraph>
       <Button {...linkProps} icon={ChevronLeft} theme="gray">
         Go Home
       </Button>
