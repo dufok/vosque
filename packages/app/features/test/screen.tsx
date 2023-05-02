1d0daaa7da6300fb211e3ff594710d2687483e4b
Stepan Vladovskiy <duffokin@gmail.com>
2023-05-02 21:49:56 +0000

    feat: tost function reparetion

--- packages/app/features/test/screen.tsx
+++ packages/app/features/test/screen.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { YStack, XStack, H1, Paragraph, Button } from "@my/ui";
 import { useLink } from "solito/link";
 import { Header } from '@my/ui/src/components/HeaderComp';
+import { useToastController } from '@tamagui/toast';
 
 export function testScreen() {
+
+  const toast = useToastController();
   
   return (
   <YStack f={1} ai="center" miw={400} space="$6">
-    < Header />
-  </YStack>
+      <Button bc="$backgroundFocus" onClick={() => {
+          toast.show('Нам нужно проверить ваш перевод!', {
+              message: "Пока мы это делаем, вы уже можете начать изучать Испанский язык!"
+          })
+      }}> Перевод выполнен ! </Button>
+    </YStack>
   );
 }
 
