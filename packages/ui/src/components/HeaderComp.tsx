f9e54858a488e36c611cef4d78793e84d0b0a584
Stepan Vladovskiy <duffokin@gmail.com>
2023-05-21 18:39:48 +0000

    fix: header take out pos

--- packages/ui/src/components/HeaderComp.tsx
+++ packages/ui/src/components/HeaderComp.tsx
@@ -11,27 +11,26 @@ export function Header() {
       jc="space-between"
       backgroundColor="$background"
       //@ts-ignore
-      pos="fixed"
       top={0}
       left={0}
       right={0}
-      zi={2}
+      zi={50}
       paddingVertical="$2"
       paddingHorizontal="$6"
       elevation={2}
       >
         <XStack space="$4" $sm={{display: "none"}} >
-          <TextLink  href="/phrasebook">
+          <TextLink prefetch={false} href="/phrasebook">
             <Paragraph >Разговорники</Paragraph>
           </TextLink>
-          <TextLink  href="/course">
+          <TextLink prefetch={false} href="/course">
             <Paragraph >Курсы</Paragraph>
           </TextLink>
         </XStack>
         <TextLink href="/">
           <img src="https://cdn.vosque.education/images/Logo.PNG?raw" alt="Vosque Logo" width="50" height="50" />
         </TextLink>
-        <TextLink href="/userpage">
+        <TextLink prefetch={false} href="/userpage">
           <Button color="$background"  backgroundColor="$backgroundPress" space="$4" >Регистрация</Button>
         </TextLink>
       </XStack>
