8394b6fb854148291bf951b4a82d97f9872f2740
Stepan Vladovskiy <duffokin@gmail.com>
2023-06-28 22:36:26 +0000

    feat: add phrasebook page and fix link on phrasebook comp

--- packages/app/features/phrasebook/screen.tsx
+++ packages/app/features/phrasebook/screen.tsx
@@ -1,22 +1,79 @@
-import { Paragraph, YStack, Button } from "@my/ui";
+import {
+  Paragraph,
+  YStack,
+  Button,
+  Image,
+  H2,
+  H3,
+  H4,
+  XStack
+} from "@my/ui";
 import React from "react";
 import { useLink } from "solito/link";
 
+import { HeaderComp } from '@my/ui/src/components/HeaderComp';
+import { SubMenu} from '@my/ui/src/components/SubMenu';
+
+
 export function phrasebookScreen() {
-  const userpageLinkProps = useLink({
-    href: "/userpage",
-  });
+  const userpageLinkProps = useLink({href: "/userpage"});
+  const phasebookLinkProps = useLink({href: "/phrasebook"});
+ 
 
   return (
-    <YStack f={1} jc="center" ai="center" space>
-      <Paragraph ta="center" fow="800">
-        Разговорники.
-      </Paragraph>
-      <YStack pt="$10" pb="$10" backgroundColor="$backgroundHover" >
-        <Button f={1} {...userpageLinkProps} theme={"gray"}>
-          ЛИЧНЫЙ КАБИНЕТ (ВОЙТИ/ЗАПИСАТЬСЯ)
-        </Button>
-      </YStack>
+    <YStack>
+      <HeaderComp />
+      <WelcomeBlock />
+      <ContentBlock />
+      <SubMenu userpageLinkProps={userpageLinkProps}/>
     </YStack>
   );
 } 
+
+export function WelcomeBlock(){
+  return(
+      <YStack mt="$10" paddingVertical="$10" ai="center" bc="$backgroundFocus" >
+          <Image mt={30} src={{uri: 'https://cdn.vosque.education/images/img-home-phrasebook.png?raw', width: 26, height: 63}}
+          height="$8"
+          width="$8"
+          scale="80%"
+          />
+          <H2 tt="uppercase" col="$background">разговорники</H2>
+          {/*<H5 fontWeight="bold" mt={50}>что это такое ?</H5>*/}
+          <Paragraph col="$background" mt="$8" ta="center" maw={800} paddingHorizontal="$8"> Мы подготовили для Вас разговорники по темам, с помощью которых Вы сможете быстро овладеть полезными словами и выражениями для знакомств, работы и повседневного общения. Каждый разговорник представляет собой удобный и компактный способ изучения лексики. PDF-документ удобен в использовании на телефоне и содержит наиболее часто используемые фразы и слова, которые помогут вам в быстром и эффективном общении.
+          </Paragraph>
+      </YStack>
+  )
+}
+
+export function ContentBlock(){
+  return(
+    <YStack ai="center">
+      <YStack marginVertical="$10" w='100%' maw={800}>
+        <H3 tt="uppercase" ta="center"> купить</H3>
+        <XStack fw="wrap" jc="center">
+          <Button w={200} h={50} m="$5" bc="$backgroundPress" onPress={() => {}}>
+            <H4 tt="uppercase"  col="$background">tinder</H4>
+          </Button>
+          <Button w={200} h={50} m="$5" bc="$backgroundPress" onPress={() => {}}>
+            <H4 tt="uppercase"  col="$background">grinder</H4>
+          </Button>
+          <Button w={200} h={50} m="$5" bc="$backgroundPress" onPress={() => {}}>
+            <H4 tt="uppercase"  col="$background">restaurante</H4>
+          </Button>
+          <Button w={200} h={50} m="$5" bc="$backgroundPress" onPress={() => {}}>
+            <H4 tt="uppercase"  col="$background">migraciones</H4>
+          </Button>
+          <Button w={200} h={50} m="$5" bc="$backgroundPress" onPress={() => {}}>
+            <H4 tt="uppercase"  col="$background">palabras <br/> malas</H4>
+          </Button>
+          <Button w={200} h={50} m="$5" bc="$backgroundPress" onPress={() => {}}>
+            <H4 tt="uppercase"  col="$background">otros</H4>
+          </Button>
+        </XStack>
+      </YStack>
+    </YStack>
+  )
+}
+
+
--- packages/ui/src/components/PhraseBooks.tsx
+++ packages/ui/src/components/PhraseBooks.tsx
@@ -1,7 +1,7 @@
 import { Paragraph, YStack, Image, H5, H2, Button } from "tamagui";
 import React from "react";
 
-export function PhraseBooks({phasebookLinkProps: {href}}){
+export function PhraseBooks({phasebookLinkProps}){
   return(
       <YStack ai="center" bc="$backgroundFocus" >
           <Image mt={30} src={{uri: 'https://cdn.vosque.education/images/img-home-phrasebook.png?raw', width: 26, height: 63}}
@@ -11,11 +11,12 @@ export function PhraseBooks({phasebookLinkProps: {href}}){
           />
           <H2 tt="uppercase" col="$background">разговорники</H2>
           {/*<H5 fontWeight="bold" mt={50}>что это такое ?</H5>*/}
-          <Paragraph mt="$8" ta="center" maw={800} paddingHorizontal="$8"> Мы подготовили для Вас разговорники по темам, с помощью которых Вы сможете быстро овладеть полезными словами и выражениями для знакомств, работы и повседневного общения. Каждый разговорник представляет собой удобный и компактный способ изучения лексики. PDF-документ удобен в использовании на телефоне и содержит наиболее часто используемые фразы и слова, которые помогут вам в быстром и эффективном общении.
+          <Paragraph col="$background" mt="$8" ta="center" maw={800} paddingHorizontal="$8"> Мы подготовили для Вас разговорники по темам, с помощью которых Вы сможете быстро овладеть полезными словами и выражениями для знакомств, работы и повседневного общения. Каждый разговорник представляет собой удобный и компактный способ изучения лексики. PDF-документ удобен в использовании на телефоне и содержит наиболее часто используемые фразы и слова, которые помогут вам в быстром и эффективном общении.
           </Paragraph>
           <YStack p={35}>
               <Button
-                mt={50} href={href}
+                mt={50}
+                {...phasebookLinkProps}
                 color="$background"
                 br={9} bw="$1" boc="$background"
                 bc='none'>
