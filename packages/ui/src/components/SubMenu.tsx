3d5f1c382a5ba7ff6579d3f7b77f057bf55ae401
Stepan Vladovskiy <duffokin@gmail.com>
2023-07-12 20:03:56 +0000

    style: SubMeny is Pretty

--- packages/ui/src/components/SubMenu.tsx
+++ packages/ui/src/components/SubMenu.tsx
@@ -1,17 +1,25 @@
 import { Paragraph, YStack, XStack, Button } from "tamagui";
 import React from "react";
 import { GithubIcon } from "./GithubIcon";
+import { InstagramIcon } from "./GithubIcon";
+import { TelegramIcon } from "./GithubIcon";
 
 export function SubMenu({userpageLinkProps}) {
   return(
     <YStack w="100%" pt="$2" pb="$2" backgroundColor="$backgroundFocus" >
-      <XStack jc="space-between">
-      <YStack>
-          <GithubIcon width={30}/>
-        </YStack>
-        <YStack>
+      <XStack f={1} w="100%" ai="center" jc="space-between">
+        <XStack alignSelf="flex-start" p="$3" space="$3">
+          <a href="https://github.com/dufok" target="_blank" rel="noopener noreferrer">
+            <GithubIcon width={30} color="$background" />
+          </a>
+          <InstagramIcon width={30} color="$background" />
+          <TelegramIcon width={30} color="$background" />
+        </XStack>
+        <YStack mr="$4">
           <Button {...userpageLinkProps} bc="backgroundPress" bw='0'>
-            ВОЙТИ/ЗАПИСАТЬСЯ
+            <Paragraph >
+              ВОЙТИ / ЗАПИСАТЬСЯ
+            </Paragraph>
           </Button>
         </YStack>
       </XStack>
