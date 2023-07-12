f42535828fed016b1133778dab04ed50e878b2dc
Stepan Vladovskiy <duffokin@gmail.com>
2023-07-12 15:53:27 +0000

    style: DopDialog block with time and progress bar

--- packages/ui/src/components/DopDialog.tsx
+++ packages/ui/src/components/DopDialog.tsx
@@ -1,11 +1,12 @@
 import {
   Paragraph,
   YStack,
-  Button,
-  ToggleGroup
+  XStack,
+  Progress
   } from 'tamagui';
 import React, { useState, useEffect } from "react";
 import { Play, Pause, Plus } from '@tamagui/lucide-icons';
+const Audio = require('react-native').Audio; // Requiring once, instead of inside the component
 
 export type Content = {
   text: string;
@@ -13,90 +14,79 @@ export type Content = {
 };
 
 interface ContentsBlockAudioProps {
-  contents: Content[];
+  contents: { [key: string]: Content };
 }
 
 export const DopDialog: React.FC<ContentsBlockAudioProps> = ({ contents }) => {
-
-  const [isPlaying, setIsPlaying] = React.useState(false);
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
-
-  const togglePlay = (src:string) => {
-    setIsPlaying(true);
-    if (audio) {
-      audio.src = src;
-      setIsPlaying(true);
-      audio.play().catch((error) => console.error(error));
-    }
-  };
-
-  const togglePause = () => {
-    if (audio) {
-      audio.pause();
-      setIsPlaying(false);
-    }
-  };
-
-  const togglePlus10sec = () => {
-    if (audio) {
-      audio.currentTime += 10;
-    }
-  };
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Audio is only available in the browser, check for window
-      setAudio(new Audio());
-    }
-  }, []);  // Run this effect only once on mount
+  const contentsArray = Object.values(contents);
 
   return (
     <YStack ai="center" mb="$4" w="90%" maw={900}>
-      {contents.map(({text, src}, index) => (
-        <YStack key={index}>
-          {activeIndex === index ? (
-            <ToggleGroup
-            orientation="horizontal"
-            id="alignment"
-            type="single"
-            size="$5"
-            m="$2"
-            backgroundColor="$borderColor"
-            >
-              <ToggleGroup.Item 
-                backgroundColor="$borderColor"
-                value="left"
-                aria-label="Left aligned">
-                <Play color='$background' onPress={() => togglePlay(src)} />
-              </ToggleGroup.Item>
-              <ToggleGroup.Item
-                backgroundColor="$borderColor"
-                value="center"
-                aria-label="Center aligned">
-                <Pause color='$background' onPress={togglePause} />
-              </ToggleGroup.Item>
-              <ToggleGroup.Item
-                backgroundColor="$borderColor"
-                value="right"
-                aria-label="Right aligned">
-                <Plus color='$background' onPress={togglePlus10sec} />
-              </ToggleGroup.Item>
-            </ToggleGroup>
-          ):(
-            <Button 
-              onPress={() => {
-              setActiveIndex(index);
-              }}
-              bc={'$borderColor'}
-              w={160} h={48}
-              m="$2"
-              >
-              <Paragraph col='$background'>{text}</Paragraph>
-            </Button>
-          )}
-        </YStack>
+      {contentsArray.map((content, index) => (
+        <AudioPlayer key={index} src={content.src} />
       ))}
     </YStack>
-  )
+  );
+};
+
+const AudioPlayer = ({ src }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio] = useState(new Audio(src));
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(audio.currentTime);
+      setDuration(audio.duration);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [audio]);
+
+  const togglePlay = () => {
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const progress = (currentTime / duration) * 100;
+
+  return (
+    <YStack
+      maw={300}
+      br="$1"
+      boc="$backgroundFocus"
+      h={20}
+      f={1}
+    >
+      <XStack>
+        {isPlaying ? (
+          <Pause onPress={togglePlay} color="$background" size="$1" />
+        ) : (
+          <Play color="$background" onPress={togglePlay} size="$1" />
+        )}
+        <Progress value={progress} f={0.2}>
+          <Progress.Indicator animation="bouncy" backgroundColor="$borderColor" f={0.5} />
+        </Progress>
+        <Paragraph color="$background" f={0.3}>
+          {formatTime(currentTime)}
+        </Paragraph>
+      </XStack>
+    </YStack>
+  );
+};
+
+// Helper function to format time in seconds into MM:SS
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
