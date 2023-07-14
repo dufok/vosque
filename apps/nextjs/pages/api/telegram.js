48ceed036a255980cf7016ce14a5fe6fb00af199
Stepan Vladovskiy <duffokin@gmail.com>
2023-07-14 14:52:41 +0000

    feat: sendTelegram message is now parcing all data iin json structure

--- apps/nextjs/pages/api/telegram.js
+++ apps/nextjs/pages/api/telegram.js
@@ -4,14 +4,21 @@ export const sendMessage = async (req, res) => {
   const { text } = req.body;
 
   try {
-    const response = await fetch(`https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_API}/sendMessage?chat_id=${process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID}&text=${text}`, {
+    const response = await fetch(`https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_API}/sendMessage`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-      }
+      },
+      body: JSON.stringify({
+        chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID,
+        text: text
+      })
     });
 
     const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.description || 'Telegram API error');
+    }
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.toString() });
