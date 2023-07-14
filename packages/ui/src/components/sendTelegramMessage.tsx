0d780eff84930118d7bbdafad30a7f01f8a33925
Stepan Vladovskiy <duffokin@gmail.com>
2023-07-14 15:14:59 +0000

    feat: sendTelegram with json catch option

--- packages/ui/src/components/sendTelegramMessage.tsx
+++ packages/ui/src/components/sendTelegramMessage.tsx
@@ -1,17 +1,21 @@
 export const sendTelegramMessage = async (text) => {
-  
+  try {
+    const response = await fetch('/api/telegram', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text }),
+    });
+
     try {
-      const response = await fetch('/api/telegram', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text }),
-      });
       const data = await response.json();
       console.log(data);
     } catch (error) {
-      console.log(error);
+      console.error('Error parsing JSON:', error);
     }
-
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
