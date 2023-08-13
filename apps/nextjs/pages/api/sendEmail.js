eb5a9d24f2a6aacd9816f0eb341c65f13aa64a50
Stepan Vladovskiy <duffokin@gmail.com>
2023-08-13 05:26:40 +0000

    fix: mailjet setup api

--- apps/nextjs/pages/api/sendEmail.js
+++ apps/nextjs/pages/api/sendEmail.js
@@ -6,27 +6,30 @@ const mj = mailjet.connect(
 );
 
 const sendEmail = async (req, res) => {
-    const { to, subject, text } = req.body;
-  
-    const request = mj.post('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: { Email: 'admin@vosque.education', Name: 'Vosque Education' },
-          To: [{ Email: to }],
-          Subject: subject,
-          TextPart: text,
-        },
-      ],
-    });
-  
-    try {
-      await request;
-      res.status(200).send('Email sent');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error sending email');
-    }
-  };
+  const { to, subject, text } = req.body;
 
-  export default sendEmail;
+  const request = mj
+    .post("send", { 'version': 'v3.1' })
+    .request({
+      "Messages": [
+        {
+          "From": { "Email": 'admin@vosque.education', "Name": 'Vosque Education' },
+          "Subject": subject,
+          "TextPart": text,
+          "To": [{ "Email": to }]
+        }
+      ]
+    });
+
+  try {
+    await request;
+    res.status(200).send('Email sent');
+  } catch (error) {
+    console.error(error.statusCode);
+    res.status(500).send('Error sending email');
+  }
+};
+
+export default sendEmail;
+
   
