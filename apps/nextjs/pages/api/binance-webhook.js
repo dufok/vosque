deab56a60f6fc3d116989fae664ad5cec545bc72
Stepan Vladovskiy <duffokin@gmail.com>
2023-09-10 23:14:53 +0000

    feat: new Prisma instance in WebHook binance

--- apps/nextjs/pages/api/binance-webhook.js
+++ apps/nextjs/pages/api/binance-webhook.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
 
   if (yourSignature !== decodedBinanceSignature) {
     console.log('Signature verification failed');
-    return res.status(401).json({ message: 'Invalid signature' }); */
+    return res.status(401).json({ message: 'Invalid signature' });
   }
   
   const paymentType = payload.bizType;
